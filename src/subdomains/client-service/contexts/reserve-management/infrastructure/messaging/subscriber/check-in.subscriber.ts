7b5a202c4ea9d393364500bba523a2c0e6c92578
Cristian Tironi <tironi741@gmail.com>
2023-03-16 12:12:30 -0300

    Persistencia de Event CheckIn
    Implementamos la persistensia de los eventos del agregado CheckIn y sus entidades en la entidad Events

--- src/subdomains/client-service/contexts/reserve-management/infrastructure/messaging/subscriber/check-in.subscriber.ts
+++ src/subdomains/client-service/contexts/reserve-management/infrastructure/messaging/subscriber/check-in.subscriber.ts
@@ -1,8 +1,13 @@
 import { Controller } from "@nestjs/common";
 import { Ctx, EventPattern, KafkaContext, Payload } from "@nestjs/microservices";
+import { EventMySqlEntity, EventService } from "../../persistence";
 
 @Controller()
 export class CheckInControllerSuscriber{
+
+    constructor(
+        private readonly eventService: EventService
+    ) { }
 
     /**
      * EventPattern se utiliza para definir un patrón de evento de Kafka
@@ -24,6 +29,14 @@ export class CheckInControllerSuscriber{
     @EventPattern('reserve-management.check-in-creted')
     checkInCreated(@Payload() data: any, @Ctx() context: KafkaContext){
 
+        
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.check-in-creted';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
+
         console.log('--------------------------------------')
         console.log('Data: ', data)
         console.log('--------------------------------------')
@@ -33,6 +46,14 @@ export class CheckInControllerSuscriber{
 
     @EventPattern('reserve-management.guest-added')
     guestAdded(@Payload() data: any, @Ctx() context: KafkaContext){
+
+                
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.guest-added';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
 
         console.log('--------------------------------------')
         console.log('Data: ', data)
@@ -44,6 +65,14 @@ export class CheckInControllerSuscriber{
     @EventPattern('reserve-management.guest-obtained')
     guestObtained(@Payload() data: any, @Ctx() context: KafkaContext){
 
+                
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.guest-obtained';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
+
         console.log('--------------------------------------')
         console.log('Data: ', data)
         console.log('--------------------------------------')
@@ -53,6 +82,14 @@ export class CheckInControllerSuscriber{
 
     @EventPattern('reserve-management.room-key-added')
     roomKeyAdded(@Payload() data: any, @Ctx() context: KafkaContext){
+
+                
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.room-key-added';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
 
         console.log('--------------------------------------')
         console.log('Data: ', data)
@@ -64,6 +101,14 @@ export class CheckInControllerSuscriber{
     @EventPattern('reserve-management.room-key-obtained')
     roomKeyObtained(@Payload() data: any, @Ctx() context: KafkaContext){
 
+                
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.room-key-obtained';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
+
         console.log('--------------------------------------')
         console.log('Data: ', data)
         console.log('--------------------------------------')
@@ -73,6 +118,14 @@ export class CheckInControllerSuscriber{
 
     @EventPattern('reserve-management.access-level-updated')
     accessLevelUpdated(@Payload() data: any, @Ctx() context: KafkaContext){
+
+                
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.access-level-updated';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
 
         console.log('--------------------------------------')
         console.log('Data: ', data)
@@ -84,6 +137,14 @@ export class CheckInControllerSuscriber{
     @EventPattern('reserve-management.email-updated')
     emailUpdated(@Payload() data: any, @Ctx() context: KafkaContext){
 
+                
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.email-updated';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
+
         console.log('--------------------------------------')
         console.log('Data: ', data)
         console.log('--------------------------------------')
@@ -93,6 +154,14 @@ export class CheckInControllerSuscriber{
 
     @EventPattern('reserve-management.phone-updated')
     phoneUpdated(@Payload() data: any, @Ctx() context: KafkaContext){
+
+                
+        const event = new EventMySqlEntity();
+        event.data = JSON.stringify(data);
+        event.type = 'reserve-management.phone-updated';
+        event.createdAt = Date();
+
+        this.eventService.registerEvent(event);
 
         console.log('--------------------------------------')
         console.log('Data: ', data)
